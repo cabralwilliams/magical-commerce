f000220c35da123122c256525df22de60f635042
Cabral Williams <cabral.williams@gmail.com>
2021-11-04 19:42:49 -0500

    went crazy and things are mostly finished

--- models/Category.js
+++ models/Category.js
@@ -13,7 +13,7 @@ Category.init(
         primaryKey: true,
         allowNull: false
     },
-    catetory_name: {
+    category_name: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
--- models/index.js
+++ models/index.js
@@ -12,12 +12,30 @@ Product.belongsTo(Category, {
     foreignKey: "category_id"
 });
 
+ProductTag.belongsTo(Tag, {
+    foreignKey: "tag_id"
+});
+
+ProductTag.belongsTo(Product, {
+    foreignKey: "product_id"
+});
+
+Tag.hasMany(ProductTag, {
+    foreignKey: "tag_id"
+});
+
+Product.hasMany(ProductTag, {
+    foreignKey: "product_id"
+});
+
 Product.belongsToMany(Tag, {
-    through: ProductTag
+    through: ProductTag,
+    foreignKey: "product_id"
 });
 
 Tag.belongsToMany(Product, {
-    through: ProductTag
+    through: ProductTag,
+    foreignKey: "tag_id"
 });
 
 module.exports = { Category, Product, Tag, ProductTag };
